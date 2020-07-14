0bd9615ab335c2cf81dc0bcf2d6e2d4a33d6c950
HernandezM-dev <HernandezM.dev@gmail.com>
2020-07-14 18:42:28 -0400

    finished project

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -5,11 +5,17 @@ import "./Posts.css";
 const Posts = (props) => {
   // Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
+  // console.log('from posts', posts);
 
   return (
     <div className="posts-container-wrapper">
       {/* map through the posts here to return a Post component */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {
+        posts.map(post =>
+          <Post post={post} likePost={likePost}/>
+        )
+      }
     </div>
   );
 };
